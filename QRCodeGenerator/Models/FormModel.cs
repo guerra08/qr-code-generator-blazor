8037732452b721531bf557ac3ce761722a4db8f8
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-11-21 01:50:11 -0300

    Adicionar arquivos de projeto.

---
+++ QRCodeGenerator/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QRCodeGenerator.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Twitter { get; set; }
+        [Required]
+        public string Github { get; set; }
+        
+    }
+}
